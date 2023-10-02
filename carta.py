bb4114b709b0bebb080e8e8bb559ccd57b41dbed
sagarcial <santiago.alexander270905@gmail.com>
2023-10-02 07:45:57 -0500

    git commit message example

---
+++ carta.py
@@ -0,0 +1,21 @@
+class Carta:
+    def __init__(self, valor, pinta):
+        self.pinta = pinta
+        self.valor = valor
+
+    def obtener_valor(self):
+        if self.valor == 'A':
+            return 1
+        if self.valor in ['J', 'Q', 'K']:
+            return 10
+        return int(self.valor)
+
+    def mostrar_carta(self):
+        return self.pinta, self.valor
+    
+class CartaFrancesa(Carta):
+    pass
+
+class CartaEspanola(Carta):
+    pass
+        
