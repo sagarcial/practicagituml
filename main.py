f43e3ed99339cbccd9ec86e72af07fc93969cf1f
sagarcial <santiago.alexander270905@gmail.com>
2023-10-11 07:03:53 -0500

    cambios

--- main.py
+++ main.py
@@ -8,31 +8,56 @@ class BlackjackGUI:
         self.root = root
         self.root.title("Blackjack")
 
-        self.canvas = tk.Canvas(root, width=800, height=600)
+        self.canvas = tk.Canvas(root, width=1500, height=950)
         self.canvas.pack()
 
         self.juego = Juego(MazoFrances())
         self.juego.iniciar_juego()
 
         self.cargar_imagenes()
-
+        
         self.jugar_button = tk.Button(root, text="Jugar", command=self.jugar)
         self.jugar_button.pack()
-
+        
         self.valorar_button = tk.Button(root, text="Valorar", command=self.valorar)
         self.valorar_button.pack()
 
     def cargar_imagenes(self):
-        self.imagenes_cartas = []
+        self.imagenes_cartas_jugador = []
+        self.imagenes_cartas_casa = []
         for carta in self.juego.jugador2.cartas:
             imagen = PhotoImage(file=f"carta_img/{carta.pinta}_{carta.valor}.png")
-            self.imagenes_cartas.append(imagen)
+            self.imagenes_cartas_jugador.append(imagen)
+
+        for carta in self.juego.jugador1.cartas:
+            imagen = PhotoImage(file=f"carta_img/{carta.pinta}_{carta.valor}.png")
+            self.imagenes_cartas_casa.append(imagen)
 
         # Muestra las imágenes de las cartas del jugador en el canvas
-        x, y = 100, 300
-        for imagen in self.imagenes_cartas:
+        x, y = 100, 700
+        for imagen in self.imagenes_cartas_jugador:
             self.canvas.create_image(x, y, image=imagen)
             x += 50  # Ajusta la posición de la siguiente carta
+
+        # Muestra las imágenes de las cartas de la casa en el canvas
+        x, y = 100, 300
+        for imagen in self.imagenes_cartas_casa:
+            self.canvas.create_image(x, y, image=imagen)
+            x += 50
+
+        # Muestra el valor de la mano del jugador
+        valor_jugador = self.juego.jugador2.obtener_valor_mazo()
+        if hasattr(self, "valor_jugador_text"):
+            self.canvas.itemconfig(self.valor_jugador_text, text=f"Valor del Jugador: {valor_jugador}")
+        else:
+            self.valor_jugador_text = self.canvas.create_text(200, 900, text=f"Valor del Jugador: {valor_jugador}")
+
+        # Muestra el valor de la mano de la casa
+        valor_casa = self.juego.jugador1.obtener_valor_mazo()
+        if hasattr(self, "valor_casa_text"):
+            self.canvas.itemconfig(self.valor_casa_text, text=f"Valor de la Casa: {valor_casa}")
+        else:
+            self.valor_casa_text = self.canvas.create_text(200, 470, text=f"Valor de la Casa: {valor_casa}")
 
     def jugar(self):
         self.juego.jugar()
@@ -40,6 +65,23 @@ class BlackjackGUI:
 
     def valorar(self):
         self.juego.valorar_juego()
+        ganador = self.determinar_ganador()
+        self.canvas.create_text(800, 400, text=f"Ganador: {ganador}", font=("Helvetica", 24))
+
+    def determinar_ganador(self):
+        valor_jugador = self.juego.jugador2.obtener_valor_mazo()
+        valor_casa = self.juego.jugador1.obtener_valor_mazo()
+
+        if valor_jugador > 21:
+            return "Casa"
+        elif valor_casa > 21:
+            return "Jugador"
+        elif valor_jugador > valor_casa:
+            return "Jugador"
+        elif valor_casa > valor_jugador:
+            return "Casa"
+        else:
+            return "Empate"
 
 if __name__ == '__main__':
     root = tk.Tk()
